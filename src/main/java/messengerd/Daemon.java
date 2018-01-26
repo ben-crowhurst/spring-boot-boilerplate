505b2a606b203c7badb304b2ad0d317aaef92450
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-01-26 15:25:01 +1100

    Bootable application.

---
+++ src/main/java/messengerd/Daemon.java
@@ -0,0 +1,11 @@
+package daemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daemon {
+    public static void main(String[] arguments) {
+        SpringApplication.run(Daemon.class, arguments);
+    }
+}
