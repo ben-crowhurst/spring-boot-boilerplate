e6c3968bebf0feee329b853ef3a78699f51012d6
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-01-27 13:31:18 +1100

    Defined Attachment class.

---
+++ src/main/java/messengerd/Attachment.java
@@ -0,0 +1,33 @@
+package messengerd;
+
+public class Attachment {
+    public Attachment() {
+        key = "";
+        content = new byte[0];
+        contentType = "";
+    }
+
+    private String key;
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String value) {
+        key = value;
+    }
+
+    private byte[] content;
+    public byte[] getContent() {
+        return content;
+    }
+    public void setContent(byte[] value) {
+        content = value;
+    }
+
+    private String contentType;
+    public String getContentType() {
+        return contentType;
+    }
+    public void setContentType(String value) {
+        contentType = value;
+    }
+}
---
+++ src/test/java/unit/AttachmentTest.java
@@ -0,0 +1,17 @@
+package messengerd;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class AttachmentTest {
+    private Attachment attachment = new Attachment();
+
+    @Test
+    public void defaultInstanceValues() {
+        assertThat(attachment.getKey(), isEmptyString());
+        assertThat(attachment.getContent(), is(new byte[0]));
+        assertThat(attachment.getContentType(), isEmptyString());
+    }
+}
